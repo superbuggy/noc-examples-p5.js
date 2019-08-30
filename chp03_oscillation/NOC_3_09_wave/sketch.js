0629f65eaa96b0be40b2e4459a80aa0f945d083a
James Reichard <jpqreichard@gmail.com>
2019-08-29 21:29:34 -0400

    Add colorcoaster

--- chp03_oscillation/NOC_3_09_wave/sketch.js
+++ chp03_oscillation/NOC_3_09_wave/sketch.js
@@ -1,22 +1,23 @@
-let startAngle = 0;
-let angleVel = 0.23;
+let startAngle = 0
+let angleVel = Math.PI / 180 / 2
 
 function setup() {
-  createCanvas(640, 360);
+  createCanvas(640, 360)
+  colorMode(HSL)
 }
 
 function draw() {
-  background(51);
+  background(51)
 
-  startAngle += 0.015;
-  let angle = startAngle;
+  startAngle += 0.015
+  let angle = startAngle
 
-  for (let x = 0; x <= width; x += 24) {
-    let y = map(sin(angle), -1, 1, 0, height);
-    stroke(164);
-    fill(255, 50);
-    strokeWeight(2);
-    ellipse(x, y, 48, 48);
-    angle += angleVel;
+  for (let x = 0; x <= width; x += 1) {
+    let y = map(sin(angle), -1, 1, height / 6, height * 5 / 6)
+    const hue = ((y % 360) + x ) % 360
+    stroke(hue, 100, 50)
+    strokeWeight(2)
+    line(x, y, x, y + 100)
+    angle += angleVel
   }
 }
